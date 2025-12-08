ce69fc5f67e05f75157f1e43e9e3d02f067fd148
tsukuyomi <samsonts1911@gmail.com>
2025-12-07 23:47:15 -0500

    Ported additional funcs to rigidbody, reduced DRY violations

--- src/physics/rigidbody.cpp
+++ src/physics/rigidbody.cpp
@@ -1,66 +1,117 @@
+#include "rigidbody.h"
+#include "utils/transform.h"
 #include <glm/gtx/quaternion.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-#include "rigidbody.h"
 
-RigidBody::RigidBody(float m, const glm::mat4& ctm)
-    : mass(m), invMass(1.f / m), initialCTM(ctm)
+RigidBody::RigidBody(PrimitiveType shapeType, float m, const glm::mat4& initialTransform)
+    : type(shapeType), mass(m), ctm(initialTransform)
 {
-    // initial position
+    reset();
+
+    // inertia tensor based on shape type
+    switch (type) {
+        case PrimitiveType::PRIMITIVE_CUBE:
+            Ibody = Transform::computeCubeInertia(mass, scale);
+            break;
+        case PrimitiveType::PRIMITIVE_SPHERE:
+            Ibody = Transform::computeSphereInertia(mass, 0.5f * scale.x);
+            break;
+        case PrimitiveType::PRIMITIVE_CYLINDER:
+            Ibody = Transform::computeCylinderInertia(mass, 0.5f * scale.x, scale.y);
+            break;
+        case PrimitiveType::PRIMITIVE_CONE:
+            Ibody = Transform::computeConeInertia(mass, 0.5f * scale.x, scale.y);
+            break;
+        default:
+            //sphere
+            Ibody = glm::mat3(1.0f);
+            break;
+    }
+
+    // Precompute inverse
+    IbodyInv = glm::inverse(Ibody);
+
+    // Compute initial auxiliary variables
+    computeAuxiliaryVariables();
+}
+
+void RigidBody::reset() {
+    // initial pos
     x = ctm[3];
 
-    // store scale to reapply later
-    initialScale = {
-        glm::length(ctm[0]),
-        glm::length(ctm[1]),
-        glm::length(ctm[2])
-    };
-
-    glm::mat3 rotMat{
-        glm::normalize(ctm[0]),
-        glm::normalize(ctm[1]),
-        glm::normalize(ctm[2])
+    // store scale
+    scale = glm::vec3{
+        glm::length(glm::vec3{ctm[0]}),
+        glm::length(glm::vec3{ctm[1]}),
+        glm::length(glm::vec3{ctm[2]})
     };
 
     // to quaternion
-    q = glm::quat_cast(rotMat);
+    q = glm::quat_cast(glm::mat3{
+                            glm::normalize(glm::vec3{ctm[0]}),
+                            glm::normalize(glm::vec3{ctm[1]}),
+                            glm::normalize(glm::vec3{ctm[2]})
+                        });
+
+    P = L = glm::vec3{0.f};
+    clearForces();
+    computeAuxiliaryVariables();
 }
 
 glm::mat4 RigidBody::getTransformMatrix() const {
     // Translation * Rotation * Scale
     glm::mat4 T = glm::translate(glm::mat4{1.f}, x);
-    glm::mat4 S = glm::scale(glm::mat4{1.f}, initialScale);
+    glm::mat4 S = glm::scale(glm::mat4{1.f}, scale);
 
     return T * R * S;
 }
 
 void RigidBody::computeAuxiliaryVariables() {
     // v(t) = P(t) / M
-    v = P * invMass;
+    v = P / mass;
 
     // quaternion to rotation matrix: R(t) = matrix(q(t))
     q = glm::normalize(q);
     R = glm::toMat4(q);
 
     glm::mat3 R3{R};
-    Iinv = R3 * IbodyInv * glm::transpose(R3);
+
+    // world space inverse inertia
+    glm::mat3 Iinv = R3 * IbodyInv * glm::transpose(R3);
 
     omega = Iinv * L;
+}
+
+void RigidBody::integrate(float dt) {
+    computeAuxiliaryVariables();
+
+    glm::quat omega_quat{0.f, omega};
+    glm::quat q_dot = 0.5f * (omega_quat * q);
+
+    // euler integration
+    x += v * dt;
+    P += force * dt;
+    L += torque * dt;
+    q += q_dot * dt;
+
+    //damping
+    P *= 0.99f;
+    L *= 0.99f;
 }
 
 void RigidBody::clearForces() {
     force = torque = glm::vec3{0.f};
 }
 
-void RigidBody::applyForce(const glm::vec3& f) {
-    force += f;
+void RigidBody::applyForce() {
+    force += mass * gravity;
 }
 
-void RigidBody::applyForceAtPoint(const glm::vec3& f, const glm::vec3& worldPoint) {
-    force += f;
+void RigidBody::applyForceAtPoint(const glm::vec3& point) {
+    applyForce();
 
     // torque
-    glm::vec3 r = worldPoint - x;
-    torque += glm::cross(r, f);
+    torque += glm::cross(point - x, mass * gravity);
 }
 
 void RigidBody::applyImpulse(const glm::vec3& impulse) {
@@ -68,31 +119,34 @@ void RigidBody::applyImpulse(const glm::vec3& impulse) {
     P += impulse;
 }
 
-// Baraff equation (5-3)
-glm::mat3 RigidBody::computeCubeInertia(float M, const glm::vec3& dim) {
-    float w = dim.x, h = dim.y, d = dim.z;
-    return glm::mat3{
-        M / 12.f * (h*h + d*d), 0.f, 0.f,
-        0.f, M / 12.f * (w*w + d*d), 0.f,
-        0.f, 0.f, M / 12.f * (w*w + h*h)
-    };
-}
+void RigidBody::bounceSphere(float groundY) {
+    if (type != PrimitiveType::PRIMITIVE_SPHERE) return;
 
-// Inertia tensor for a sphere
-glm::mat3 RigidBody::computeSphereInertia(float M, float r) {
-    return glm::scale(glm::mat4{1.f}, glm::vec3{(2.f / 5.f) * M * r*r});
-}
+    // sphere radius
+    float radius = scale.x * 0.5f;
 
-// i tensor for a cylinder along Y axis
-glm::mat3 RigidBody::computeCylinderInertia(float M, float r, float h) {
-    float Ixx = M * (3 * r*r + h*h) / 12.f;
-    float Iyy = M * r*r / 2.f;
-    return glm::scale(glm::mat4{1.f}, {Ixx, Iyy, Ixx});
-}
+    // if sphere hit ground
+    float bottomY = x.y - radius;
 
-// i tensor for a cone along Y axis
-glm::mat3 RigidBody::computeConeInertia(float M, float r, float h) {
-    float Ixx = M * (3 * r*r / 20.f + h*h / 10.f);
-    float Iyy = 3 * M * r*r / 10.f;
-    return glm::scale(glm::mat4{1.f}, {Ixx, Iyy, Ixx});
+    if (bottomY <= groundY) {
+        x.y = groundY + radius;
+
+        // bounciness
+        float restitution = 0.7f;
+
+        if (v.y < 0) {
+            v.y = -v.y * restitution;
+
+            // new linear momentum to match new velocity
+            P = mass * v;
+
+            // reduce horizontal velocity on impact
+            v.x *= 0.9f;
+            v.z *= 0.9f;
+            P = mass * v;
+
+            // reduce spin on impact
+            L *= 0.95f;
+        }
+    }
 }
--- src/physics/rigidbody.h
+++ src/physics/rigidbody.h
@@ -5,20 +5,23 @@
 #include <glm/gtc/quaternion.hpp>
 #include "utils/scenedata.h"
 
-struct Force {
-    float impulse;
-    glm::vec3 v;
-
-    static constexpr float g = 9.8f;
-};
-
 class RigidBody {
 public:
-    RigidBody() {}
+    RigidBody(PrimitiveType shapeType, float m, const glm::mat4& initialTransform);
 
-    RigidBody(float mass, const glm::mat4& initialTransform);
+    void reset();
 
     glm::mat4 getTransformMatrix() const;
+
+    void clearForces();
+
+    void applyForce();
+
+    void integrate(float dt);
+
+    void bounceSphere(float groundY);
+
+    bool enableGravity = false;
 
 private:
     glm::vec3 x{0.f};                   // position x(t) - center of mass
@@ -26,7 +29,6 @@ private:
     glm::vec3 P{0.f};                   // linear momentum P(t)
     glm::vec3 L{0.f};                   // angular momentum L(t)
     float mass = 1.f;                   // m
-    float invMass = 1.f;                // 1/m
 
     // derived state
     glm::vec3 v{0.f};                   // linear velocity v(t) = P(t) / M
@@ -36,25 +38,18 @@ private:
     glm::vec3 torque{0.f};              // torque
     glm::mat3 Ibody{1.f};               // inertia tensor
     glm::mat3 IbodyInv{1.f};            // inverse of inertia tensor
-    glm::mat3 Iinv{1.f};                // world space inverse inertia
     glm::mat4 R{1.f};                   // rotation matrix (from quaternion)
-    glm::mat4 initialCTM{1.f};
-    glm::vec3 initialScale{1.f};
+    glm::mat4 ctm{1.f};
+    glm::vec3 scale{1.f};
 
-    bool isStatic = false;
-    bool enableGravity = true;
-    PrimitiveType shapeType;
+    PrimitiveType type;
 
     void computeAuxiliaryVariables();
-    void clearForces();
-    void applyForce(const glm::vec3& f);
-    void applyForceAtPoint(const glm::vec3& f, const glm::vec3& worldPoint);
+
+    void applyForceAtPoint(const glm::vec3& point);
     void applyImpulse(const glm::vec3& impulse);
 
-    static glm::mat3 computeCubeInertia(float M, const glm::vec3& dim);
-    static glm::mat3 computeSphereInertia(float M, float r);
-    static glm::mat3 computeCylinderInertia(float M, float r, float h);
-    static glm::mat3 computeConeInertia(float M, float r, float h);
+    constexpr static glm::vec3 gravity{0.f, -9.8f, 0.f};
 };
 
 #endif // RIGIDBODY_H
